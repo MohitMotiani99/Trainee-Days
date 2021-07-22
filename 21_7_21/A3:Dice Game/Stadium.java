973a52f938d30a46c0a61257baec65670a91c682
Mohit Motiani <34206001+MohitMotiani99@users.noreply.github.com>
2021-07-23 02:45:46 +0530

    Update Stadium.java

--- 21_7_21/A3:Dice Game/Stadium.java
+++ 21_7_21/A3:Dice Game/Stadium.java
@@ -5,11 +5,11 @@ import java.util.Random;
 public class Stadium {
 
 	
+	@SuppressWarnings("unused")
 	private int attendance;
 	private int playerCount;
 	private Player[] players;
 	private int[] scores;
-	private int minV,maxV;
 	
 
 	public Stadium(int attendance, int playerCount,int minV,int maxV) {
@@ -17,20 +17,22 @@ public class Stadium {
 		this.attendance = attendance;
 		this.playerCount = playerCount;
 		this.players=new Player[playerCount];
-		this.scores=new int[playerCount];
-		this.minV=minV;
-		this.maxV=maxV;
-	}
-	public int[] generatePlayers() {
+
 		Random rand=new Random();
-		int[] pids=new int[playerCount];
-		
+
 		for(int i=0;i<playerCount;i++) {
 			int num=rand.nextInt(attendance-1)+1;
 			players[i]=new Player(num,minV,maxV);
-			pids[i]=num;
 		}
 		
+		this.scores=new int[playerCount];
+		
+	}
+	public int[] generatePlayers() {
+		int[] pids=new int[playerCount];
+		for(int i=0;i<playerCount;i++) {
+			pids[i]=players[i].getpNum();
+		}
 		return pids;
 		
 	}
